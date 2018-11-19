2ea380d0104e69d57167d18e8b3130863d4b12a3
IMRAlexI <balym1999@gmail.com>
2018-11-19 10:15:36 +0200

    Updated version with improved naming no more "filling"! 19.11.2018

--- CALCULATOR_FINAL_1/inputs.py
+++ CALCULATOR_FINAL_1/inputs.py
@@ -1,6 +1,6 @@
-﻿import actions as act
+import actions as act
 
-def input_std_math(filling):
+def input_std_math(flag):
 
     operation = {
     "+" : act.add,
@@ -9,7 +9,7 @@ def input_std_math(filling):
     "/" : act.div
                 }
 
-    while filling:
+    while flag:
         try:
             first_num = float(input("   Введите число >>> "))
             oper = input("   Введите операцию >>> ")
@@ -19,17 +19,17 @@ def input_std_math(filling):
             print("\n   Вы ввели число некоректно, повторите ввод!")
             continue
         else:
-            filling = 0
+            flag = 0
         return operation[oper](first_num, second_num)
 
-def input_geom_math(filling):
+def input_geom_math(flag):
     operation = {
         "sin" : act.geom_sin,
         "cos" : act.geom_cos,
         "tg" : act.geom_tg,
         "ctg" : act.geom_ctg
     }
-    while filling:
+    while flag:
         try:
             oper = input("   Введите операцию(sin, cos, tg, ctg) >>> ")
             oper = oper.replace(" ", "")
@@ -38,11 +38,11 @@ def input_geom_math(filling):
             print("\n   Вы выполнили ввод некоректно, повторите ввод!")
             continue
         else:
-            filling = 0
+            flag = 0
         return operation[oper](geom_num)
 
-def input_pow_math(filling):
-    while filling:
+def input_pow_math(flag):
+    while flag:
         try:
             pow_num = float(input("   Введите число >>>"))
             pow_value = float(input("   Введите числовое значение степени >>>"))
@@ -50,11 +50,11 @@ def input_pow_math(filling):
             print("   Вы ввели число некоректно, повторите ввод!")
             continue
         else:
-            filling = 0
+            flag = 0
             return act.pow(pow_num, pow_value)
 
-def input_root_math(filling):
-    while filling:
+def input_root_math(flag):
+    while flag:
         try:
             root_num = float(input("   Введите число >>>"))
             root_value = float(input("   Введите числовое значение корня >>>"))
@@ -62,5 +62,5 @@ def input_root_math(filling):
             print("   Вы ввели число некоректно, повторите ввод!")
             continue
         else:
-            filling = 0
+            flag = 0
         return act.root(root_num, root_value)
